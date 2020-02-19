1ef314dda23d2cb60524f01eaaafba1f45e908cb
pbuszczyk <40237119+pbuszczyk@users.noreply.github.com>
2020-02-19 15:52:16 +0100

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -1,2 +1,8 @@
 #/bin/bash
 sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+curl https://raw.githubusercontent.com/pbuszczyk/docker/master/dockerfile -o dockerfile
+docker build -t hello-world .
+docker run -t -i -p 80:80 hello-world
